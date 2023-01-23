3ad1eb0e584d7dceaedaa483664adbe362e435b2
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-23 20:04:26 -0300

    Creación de One-To-One (Proyecto Restaurant)

---
+++ onetoone/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def create(request):
+    # TODO Crear Elementos
+    return HttpResponse("Datos creados")
+    
