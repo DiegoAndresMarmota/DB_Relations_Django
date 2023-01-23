bf11ab6bbbab14ad37335fa1d0870e7c028f3b44
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-23 20:59:26 -0300

    Creación app Many to one

--- relations/urls.py
+++ relations/urls.py
@@ -16,4 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("one/", include("onetoone.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("one/", include("onetoone.urls")),
+    path("many/", include("manytoone.urls")),
+]
