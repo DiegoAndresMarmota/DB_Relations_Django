410a0a973007f1f0901f5ad662e04837899db361
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-23 20:25:30 -0300

    Creación de la primera relación 1-to-1

--- onetoone/views.py
+++ onetoone/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Place, Restaurant
 
 # Create your views here.
 
+
 def create(request):
-    # TODO Crear Elementos
-    return HttpResponse("Datos creados")
-    
+    place = Place(name="Donde La Aurora", address="Calle Vicente Cortes 512")
+    place.save()
+
+    restaurant = Restaurant(place=place, number_of_employees=8)
+    restaurant.save()
+
+    return HttpResponse(restaurant.place.name)
