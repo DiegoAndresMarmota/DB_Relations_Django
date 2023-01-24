0b5b450ba71ea5a824302153cea3b20660766d8e
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-23 21:13:14 -0300

    Creación de modelos

--- manytoone/views.py
+++ manytoone/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Reporter, Article
+from datetime import date
 
 # Create your views here.
 
 
 def create(request):
-    return HttpResponse("Creando relación uno a muchos")
+    rep = Reporter(first_name="Diego", last_name="Solis", email="diego_solis@gmail.com")
+    rep.save()
+    art1 = Article(
+        headline="Lorem ipsum dolot", pub_date=date(2022, 5, 5), reporter=rep
+    )
+    art1.save()
+    art2 = Article(
+        headline="Lorem ipsum dorime", pub_date=date(2021, 10, 7), reporter=rep
+    )
+    art2.save()
+    art3 = Article(
+        headline="Lorem ipsum aimet", pub_date=date(2023, 1, 2), reporter=rep
+    )
+    art3.save()
+
+    result = art1.reporter.first_name
+
+    return HttpResponse(result)
