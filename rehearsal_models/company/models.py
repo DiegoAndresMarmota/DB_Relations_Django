dc90f6050730b90c57f57b9099bb87b5747243a3
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-24 22:31:44 -0300

    Vinculación a través de ForeignKey y migración

--- rehearsal_models/company/models.py
+++ rehearsal_models/company/models.py
@@ -1,23 +1,14 @@
 from django.db import models
 
+# # Relaciones y diseño del modelo de datos.
+
+# Employee(id) >---> Job(id)
+# Employee(id) >---> Place(id)
+# Job(id) >---> Salary(id)
+# Place(id) >---> Location(id)
+# Location(id >---> Country(id)
+
 # Create your models here.
-class Employee(models.Model):
-    id_number = models.CharField(max_length=10, blank=False, null=False)
-    name = models.CharField(max_length=30, blank=False, null=False)
-    last_name = models.CharField(max_length=30, blank=False, null=False)
-    email = models.EmailField(max_length=50, blank=False, null=False)
-    address = models.CharField(max_length=50, blank=False, null=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Job(models.Model):
-    title = models.CharField(max_length=20, blank=False, null=False)
-    description = models.TextField(null=True)
-
-    def __str__(self):
-        return self.title
 
 
 class Salary(models.Model):
@@ -29,10 +20,18 @@ class Salary(models.Model):
         return self.amount
 
 
-class Place(models.Model):
-    name = models.CharField(max_length=30, blank=False, null=False)
-    address = models.CharField(max_length=70, blank=False, null=False)
-    zip_code = models.CharField(max_length=7, blank=False, null=False)
+class Job(models.Model):
+    title = models.CharField(max_length=20, blank=False, null=False)
+    description = models.TextField(null=True)
+    salary = models.ForeignKey(Salary, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    country_code = models.CharField(max_length=7, blank=False, null=False)
 
     def __str__(self):
         return self.name
@@ -40,14 +39,30 @@ class Place(models.Model):
 
 class Location(models.Model):
     name = models.CharField(max_length=40, blank=False, null=False)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 
-class Country(models.Model):
-    name = models.CharField(max_length=20, blank=False, null=False)
-    country_code = models.CharField(max_length=7, blank=False, null=False)
+class Place(models.Model):
+    name = models.CharField(max_length=30, blank=False, null=False)
+    address = models.CharField(max_length=70, blank=False, null=False)
+    zip_code = models.CharField(max_length=7, blank=False, null=False)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Employee(models.Model):
+    id_number = models.CharField(max_length=10, blank=False, null=False)
+    name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    email = models.EmailField(max_length=50, blank=False, null=False)
+    address = models.CharField(max_length=50, blank=False, null=False)
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
