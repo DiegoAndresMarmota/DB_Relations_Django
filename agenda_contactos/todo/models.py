918702bf573b51bdf174c71aaa9a24652bda18a6
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-01-29 17:55:59 -0300

    Creación de models.contact y models.todo

--- agenda_contactos/todo/models.py
+++ agenda_contactos/todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=150, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    date = models.DateField(default=date.today)
+    estimated_end = models.DateField(blank=True, null=True)
+    priority = models.IntegerField(default=3)
+
+    def __str__(self):
+        return self.title
