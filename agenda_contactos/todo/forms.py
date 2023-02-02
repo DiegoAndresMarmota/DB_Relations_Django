b78df9ab0065a3d64df00575ffba322e66f9bc17
Diego Andres Echalar <99225490+DiegoAndresMarmota@users.noreply.github.com>
2023-02-02 15:36:28 -0300

    Utilización de Calendario en Productos

--- agenda_contactos/todo/forms.py
+++ agenda_contactos/todo/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from .models import Todo
 
 
@@ -6,3 +6,6 @@ class TodoForm(ModelForm):
     class Meta:
         model = Todo
         exclude = ("date",)
+        widgets = {
+            "estimated_end": DateInput(attrs={"type": "date"}),
+        }
